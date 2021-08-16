89c951fe0efcda11a14c655779f5db040319da5d
yiğitcan Öztürk <yozturk037@gmail.com>
2021-08-16 13:54:19 +0300

    delete project

--- src/App.vue
+++ src/App.vue
@@ -28,15 +28,35 @@
 
 <script>
 
-import firebase from '../src/firebase/config'
+import firebase from "firebase";
 
+import 'firebase/storage';
+import 'firebase/firestore';
+
+const config = {
+    apiKey: "AIzaSyDZsdkJBQTPGkRlM8twcwx06pvY8cd6ouU",
+    authDomain: "books-e8d30.firebaseapp.com",
+    databaseURL: "https://books-e8d30-default-rtdb.firebaseio.com",
+    projectId: "books-e8d30",
+    storageBucket: "books-e8d30.appspot.com",
+    messagingSenderId: "979938154021",
+    appId: "1:979938154021:web:96bdba4d18a86cedc051cc",
+    measurementId: "G-60GMG41Y4F"
+};
+
+firebase.initializeApp(config);
+
+const app = firebase.initializeApp(config);
+
+
+var usersRef = app.database().ref("books");
 
 
 
 export default {
   name: "App",
   firebase: {
-    books: bookRef,
+    books: usersRef,
   },
 };
 </script>
