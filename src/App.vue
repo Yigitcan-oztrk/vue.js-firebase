ad53d947e31122cd22ab482da23f89332cb0527b
yiğitcan Öztürk <yozturk037@gmail.com>
2021-08-16 16:52:22 +0300

    update true code

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,65 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app" class="container">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="card">
+      <div class="card-header">
+        <h3>Randevular</h3>
+      </div>
+      <div class="card-body">
+        <table class="table">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">İsim</th>
+              <th scope="col">Konu</th>
+              <th scope="col">İşlem</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="book in books">
+              <td>{{book.name}}</td>
+              <td>{{book.subject}}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZsdkJBQTPGkRlM8twcwx06pvY8cd6ouU",
+  authDomain: "books-e8d30.firebaseapp.com",
+  databaseURL: "https://books-e8d30-default-rtdb.firebaseio.com",
+  projectId: "books-e8d30",
+  storageBucket: "books-e8d30.appspot.com",
+  messagingSenderId: "979938154021",
+  appId: "1:979938154021:web:96bdba4d18a86cedc051cc",
+  measurementId: "G-60GMG41Y4F",
+};
+
+let app = firebase.initializeApp(firebaseConfig);
+let db = app.database();
+let bookRef = db.ref("books")
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  firebase: {
+    books: bookRef,
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
+      msg: "Randevu listesine Hoş Geldiniz",
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -40,7 +67,8 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+
+import { firestorePlugin } from 'vuefire'
+
+
 import App from './App.vue'
+
+Vue.use(firestorePlugin)
 
 new Vue({
   el: '#app',
